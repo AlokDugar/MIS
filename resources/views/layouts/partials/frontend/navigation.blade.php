89f75d2a9a0fccf5ba9efb7f187fd1ea46db5b9d
AlokDugar <alokdugar4@gmail.com>
2025-10-15 16:18:43 +0545

    clean code

--- resources/views/layouts/partials/frontend/navigation.blade.php
+++ resources/views/layouts/partials/frontend/navigation.blade.php
@@ -26,8 +26,6 @@
             </li>
         </ul>
 
-
-
         <button class="nav-toggle" id="navToggle">
             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                 <path d="M3 12h18M3 6h18M3 18h18"></path>
